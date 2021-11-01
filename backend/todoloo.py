36a5e57da795f79314cebb0096eb9d2379aafc78
icereg1992 <willwill.sullivan@gmail.com>
2021-11-01 02:56:50 -0500

    Backend dumps data to pickle file

--- backend/todoloo.py
+++ backend/todoloo.py
@@ -1,12 +1,19 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import json
 import pickle
 
 app = Flask(__name__)
 CORS(app)
 
-data = {}
+def save_datafile():
+    datafile = open('mem.pkl', 'wb')
+    pickle.dump(data, datafile)
+    datafile.close()
+
+datafile = open('mem.pkl', 'rb')
+data = pickle.load(datafile)
+datafile.close()
+
 
 @app.route("/")
 def hello_world():
@@ -17,6 +24,7 @@ def todos(uid):
     if request.method == 'POST':
         data[uid] = request.data
         response = jsonify("Looks good to me!")
+        save_datafile()
         return (response, 200)
     else:
         if uid not in data.keys():
